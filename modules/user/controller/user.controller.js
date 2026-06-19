52dccfea635a790dc6ddb0e213b0690431cfad44
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-19 23:12:39 +0330

    add full crud

--- modules/user/controller/user.controller.js
+++ modules/user/controller/user.controller.js
@@ -1,17 +1,79 @@
-const pool = require('./../../../db');
+const UserService = require('./../service/user.service');
 
-exports.createUser = async (req, res) => {
-  const { name, email } = req.body;
-  const result = await pool.query(
-    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
-    [name, email],
-  );
+exports.createUser = async (req, res, next) => {
+  try {
+    const { name, email } = req.body;
 
-  res.status(201).json({ message: 'Create user', user: result.rows[0] });
+    const user = await UserService.createUser(
+      name,
+      email
+    );
+
+    res.status(201).json({
+      status: 'success',
+      user
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.getAllUsers = async (req, res) => {
-  const result = await pool.query('SELECT * FROM users');
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await UserService.getAllUsers();
 
-  res.status(200).json({ message: 'Get all users', users: result.rows });
+    res.status(200).json({
+      status: 'success',
+      results: users.length,
+      users
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getUserById = async (req, res, next) => {
+  try {
+    const user = await UserService.getUserById(
+      req.params.id
+    );
+
+    res.status(200).json({
+      status: 'success',
+      user
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    const { name, email } = req.body;
+
+    const user = await UserService.updateUser(
+      req.params.id,
+      name,
+      email
+    );
+
+    res.status(200).json({
+      status: 'success',
+      user
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    await UserService.deleteUser(req.params.id);
+
+    res.status(204).json({
+      status: 'success'
+    });
+  } catch (err) {
+    next(err);
+  }
 };
